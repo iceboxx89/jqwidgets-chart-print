20d377e40d9bea554fb3c2675fc002c046c9317d
Jagadish Kumar Patnala <jag@Jagadishs-MacBook-Pro.local>
2020-10-30 14:35:06 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { jqxChartComponent } from 'jqwidgets-ng/jqxchart';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'jqwidgets-pdf';
+  private employeeData: any[] = [];
+  @ViewChild(jqxChartComponent, {static: false}) chart: jqxChartComponent;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.httpClient.get('http://dummy.restapiexample.com/api/v1/employees').subscribe((response: any) => {
+      console.log(response);
+      this.employeeData = response.data;
+      const chartOptions: jqwidgets.ChartOptions = {
+        title: '',
+        description: '',
+        enableAnimations: true,
+        renderEngine: 'auto',
+        source: this.employeeData,
+        xAxis: {
+          dataField: 'employee_age'
+        },
+        seriesGroups: [
+          {
+            type: 'column',
+            series: [
+              {dataField: 'employee_salary', displayText: 'Salary', labels: {visible: false}, useGradientColors: false, fillColor: 'blue'}
+            ]
+          }
+        ]
+      };
+      this.chart.createComponent(chartOptions);
+    });
+  }
+
+  generatePdf() {
+    html2canvas(document.getElementById('myChart')).then(
+      (canvas) => {
+        const pdf = new jsPDF('p', 'mm', 'a4');
+        pdf.addImage(canvas.toDataURL('png'), 'PNG', 0, 0, 200, 200);
+        pdf.output('dataurlnewwindow');
+        // const elem = document.getElementById('myImage');
+        // elem.setAttribute('src', canvas.toDataURL('image/png'));
+      }
+    );
+  }
+
 }
